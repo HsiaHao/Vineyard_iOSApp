0e7698fe12dd4f2938fb88a3d20894dd44f3765b
Ying-Hao <hsiayinghao1818@gmail.com>
2021-05-02 21:55:25 -0400

    User Interface

---
+++ VIdeo_Camera/SimpleVideoCam/Controller/HomeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeViewController.swift
+//  SimpleVideoCam
+//
+//  Created by 夏英浩 on 5/2/21.
+//  Copyright © 2021 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func newVIdeoPressed(_ sender: UIButton) {
+        print("new video")
+    }
+    @IBAction func showVideoList(_ sender: UIButton) {
+        print("List")
+    }
+    @IBAction func showTutorial(_ sender: UIButton) {
+        print("tutorial")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VIdeo_Camera/SimpleVideoCam/Controller/PickerViewController.swift
@@ -0,0 +1,116 @@
+//
+//  PickerViewController.swift
+//  SimpleVideoCam
+//
+//  Created by 夏英浩 on 5/2/21.
+//  Copyright © 2021 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class PickerViewController: UIViewController {
+
+    @IBOutlet weak var plotPicker: UIPickerView!
+    @IBOutlet weak var varietyPicker: UIPickerView!
+    @IBOutlet weak var rowPicker: UIPickerView!
+    
+    var plotPickerData: [String] = []
+    var varietyPickerData: [String] = []
+    var rowPickerData: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+        
+        title = "Vineyard"
+        
+        plotPicker.tag = 1
+        varietyPicker.tag = 2
+        rowPicker.tag = 3
+        
+        plotPicker.setValue(UIColor.white, forKey: "textColor")
+        varietyPicker.setValue(UIColor.white, forKey: "textColor")
+        rowPicker.setValue(UIColor.white, forKey: "textColor")
+        
+        setDelegate()
+        setPickerData()
+    }
+    
+    func setPickerData(){
+        for i in 1...30{
+            plotPickerData.append(String(i))
+        }
+        
+        for i in 1...20{
+            rowPickerData.append(String(i))
+        }
+        
+        varietyPickerData = ["New York Muscat", "Van Buren Grapevine", "Alden", "Amsden", "Autumn Royal"]
+    }
+    
+    func setDelegate(){
+        self.plotPicker.delegate = self
+        self.plotPicker.dataSource = self
+        
+        self.varietyPicker.delegate = self
+        self.varietyPicker.dataSource = self
+        
+        self.rowPicker.delegate = self
+        self.rowPicker.dataSource = self
+        
+    }
+
+}
+
+extension PickerViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        if pickerView.tag==1{
+            // plot
+            if component==1{
+                return plotPickerData.count
+            }else{
+                return 1
+            }
+        }else if pickerView.tag==2{
+            //variety
+            if component==1{
+                return varietyPickerData.count
+            }else{
+                return 1
+            }
+        }else{
+            //row
+            if component==1{
+                return rowPickerData.count
+            }else{
+                return 1
+            }
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag==1{
+            if component==0{
+                return "Plot:"
+            }else{
+                return plotPickerData[row]
+            }
+        }else if pickerView.tag==2{
+            if component==0{
+                return "Variety:"
+            }else{
+                return varietyPickerData[row]
+            }
+        }else{
+            if component==0{
+                return "Row:"
+            }else{
+                return rowPickerData[row]
+            }
+        }
+    }
+}
